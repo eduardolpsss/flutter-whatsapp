ea47c64033cd4dbb609831b7e6b08e775ebac337
Eduardo Pontes <eduardo.pontes2801@gmail.com>
2023-04-18 08:58:45 -0300

    feat/loginPageAndFirebaseLink

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  //Inicializando o Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCCjd09xbB9uhLdcUb5IwY1iii7clzhyuQ",
+      appId: "1:110274997235:web:20ebbb156e2f7fe0bdc086",
+      messagingSenderId: "110274997235",
+      projectId: "whatsapp-web-18168",
+    ),
+  );
   runApp(const MaterialApp(
     title: 'WhatsApp',
     debugShowCheckedModeBanner: false,
